a3a2dff7454386af9f00fa1d5de4a206e127905b
AndyJrII <enehizenajames@gmail.com>
2024-01-25 08:46:35 +0100

    Service_Details renamed to Service, added to Admin

--- swbc/admin.py
+++ swbc/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import NewsEvent, Sermon
+from .models import NewsEvent, Sermon, Service
 
 admin.site.register(NewsEvent)
 admin.site.register(Sermon)
+admin.site.register(Service)
--- swbc/models.py
+++ swbc/models.py
@@ -4,10 +4,10 @@ class NewsEvent(models.Model):
     title = models.CharField(max_length=255)
     date_published = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(null=True)
     feature_pic = models.ImageField(upload_to='newsevent_images', default='feature_default.jpg')
-    content_pic1 = models.ImageField(upload_to='newsevent_images')
-    content_pic2 = models.ImageField(upload_to='newsevent_images')
+    content_pic1 = models.ImageField(upload_to='newsevent_images', default='content_default.jpg')
+    content_pic2 = models.ImageField(upload_to='newsevent_images', default='content_default.jpg')
     unique_together = [title,date_published]
 
     def __str__(self):
@@ -21,17 +21,17 @@ class Sermon(models.Model):
     date_preached = models.DateField(null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    unique_together = [title,author]
     feature_pic = models.ImageField(upload_to='sermon_images', default='feature_default.jpg')
+    unique_together = [title,author]
 
     def __str__(self):
         return f'{self.title}'
 
-class Service_Details(models.Model):
+class Service(models.Model):
     title = models.CharField(max_length=255, unique=True)
     details = models.TextField(blank=True)
     feature_pic = models.ImageField(upload_to='service_images', default='feature_default.jpg')
-    content_pic = models.ImageField(upload_to='service_images')
+    content_pic = models.ImageField(upload_to='service_images', default='content_default.jpg')
 
     def __str__(self):
         return f'{self.title}'
