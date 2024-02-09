a5403a62f62a10e2c58b12c5b47e3190c84b9605
AndyJrII <enehizenajames@gmail.com>
2024-02-09 17:36:49 +0100

    bookstore views & urls sorted

--- bookstore/views.py
+++ bookstore/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
+def bookstore(request):
     # pylint: disable=no-member
-    # latest_newsevents = NewsEvent.objects.order_by("-date_updated")[:3]
-    # services = Service.objects.all()
-    # context = {"latest_newsevents": latest_newsevents, "services": services}
-    template = loader.get_template("bookstore.html")
-    return HttpResponse(template.render(request))
+    name = {"name": "Andy James", "sex": "Male"}
+    context = {"name": name}
+    template = loader.get_template("books.html")
+    return HttpResponse(template.render(context, request))
