d4066d93851c75cf78ef14d6df5978d1677836eb
AndyJrII <enehizenajames@gmail.com>
2024-02-09 19:12:00 +0100

    Created Bookstore model

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cover_image = models.ImageField(upload_to="book_covers/")
+    book_file = models.FileField(upload_to="book_files/")
